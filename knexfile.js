52d0db560bc824c378d91b537d6779c2e0cbfd0e
umbur <work1415@gmail.com>
2019-02-27 15:39:32 -0700

    initial commit, plus installed dependencies

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/lambda.sqlite3'
+    },
+    useNullAsDefault: true,
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      // funs after a connection is made to the sqlite engine
+      conn.run('PRAGMA foreign_keys = ON', done);
+    },
+  },
+  migrations: {
+    directory: './data/migrations',
+    tableName: 'knex_migrations',
+  },
+  seeds: {
+    directory: './data/seeds',
+  },
+};
